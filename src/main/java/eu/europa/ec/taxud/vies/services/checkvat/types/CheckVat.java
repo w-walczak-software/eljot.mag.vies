16501c0fbfcc6bead5e700e5ba9901b557913098
CORPNET\wowalczak <wowalczak@sygnity.pl>
2023-03-01 14:11:57 +0100

    init

---
+++ src/main/java/eu/europa/ec/taxud/vies/services/checkvat/types/CheckVat.java
@@ -0,0 +1,91 @@
+package eu.europa.ec.taxud.vies.services.checkvat.types;
+
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="countryCode" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *         &lt;element name="vatNumber" type="{http://www.w3.org/2001/XMLSchema}string"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "countryCode",
+    "vatNumber"
+})
+@XmlRootElement(name = "checkVat")
+public class CheckVat {
+
+    @XmlElement(required = true)
+    protected String countryCode;
+    @XmlElement(required = true)
+    protected String vatNumber;
+
+    /**
+     * Gets the value of the countryCode property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    /**
+     * Sets the value of the countryCode property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCountryCode(String value) {
+        this.countryCode = value;
+    }
+
+    /**
+     * Gets the value of the vatNumber property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getVatNumber() {
+        return vatNumber;
+    }
+
+    /**
+     * Sets the value of the vatNumber property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setVatNumber(String value) {
+        this.vatNumber = value;
+    }
+
+}
---
+++ src/main/java/eu/europa/ec/taxud/vies/services/checkvat/types/package-info.java
@@ -0,0 +1,2 @@
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "urn:ec.europa.eu:taxud:vies:services:checkVat:types", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package eu.europa.ec.taxud.vies.services.checkvat.types;
